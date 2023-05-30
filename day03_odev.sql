2b86700180cdd5dc22e0a2d3943a0f538543260f
sule2023 <ysule1014@gmail.com>
2023-05-30 10:07:15 +0200

    son

--- day03_odev.sql
+++ day03_odev.sql
@@ -2,21 +2,45 @@
 /*-------------------------------------------------------------------------
 Q1: 3 harfli kelimeleri listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where kelime like'___';
 /*-------------------------------------------------------------------------
 Q2: En az 3 harfli kelimeleri listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where kelime like'___%';
 /*-------------------------------------------------------------------------
 Q3: H ile baslayip t ile biten kelimeleri listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where kelime like'H%t';
 /*-------------------------------------------------------------------------
 Q4: H ile baslayip t ile biten kelimeleri case sensitive listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where regexp_like (kelime , '^H.*t$','c');
+
+insert into kelimeler values(1021,'Hoot',4);
 /*-------------------------------------------------------------------------
 Q5: H ile baslayip t ile biten 3 harfli kelimeleri case sensitive listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where regexp_like (kelime , '^H[!-~]T$','c');
 /*-------------------------------------------------------------------------
 Q6: 3 ya da daha az karakterli kelimeleri listeleyin
 ---------------------------------------------------------------------------*/
+select kelime 
+from kelimeler
+where kelime not like '____%';
 /*-------------------------------------------------------------------------
 Q7: id'si 8 rakamini iceren kelimeleri listeleyin
 ---------------------------------------------------------------------------*/
+SELECT id
+FROM kelimeler
+WHERE id LIKE '%8%'
+
--- day03_odev2.sql
+++ day03_odev2.sql
@@ -3,6 +3,14 @@
  Icinde id,isim,veli_isim ve grade field'lari olsun. 
  Id field'i Primary Key olsun.
  --------------------------------------------------------------------------*/
+  create table lise
+  (
+  id int primary key,
+  isim varchar (20),
+  veli_isim varchar(20),
+  grade int
+  
+  );
  /*-------------------------------------------------------------------------
  2)  Kayitlari tabloya ekleyin.
  (123, 'Ali Can', 'Hasan',75), 
@@ -13,11 +21,27 @@
  (128, 'Resul Can', 'Tugay',85);
  (129, 'Tugay Kala', 'Osman',45);
  --------------------------------------------------------------------------*/
+ insert into lise values
+ (123, 'Ali Can', 'Hasan',75), 
+ (124, 'Merve Gul', 'Ayse',85), 
+ (125, 'Kemal Yasa', 'Hasan',85),
+ (126, 'Rumeysa Aydin', 'Zeynep',85),
+ (127, 'Oguz Karaca', 'Tuncay',85),
+ (128, 'Resul Can', 'Tugay',85),
+ (129, 'Tugay Kala', 'Osman',45);
  /*-------------------------------------------------------------------------
 3)deneme_puani tablosu olusturun. 
 ogrenci_id, ders_adi, yazili_notu field'lari olsun, 
 ogrenci_id field'i Foreign Key olsun 
 --------------------------------------------------------------------------*/
+create table deneme_puani
+(
+ogrenci_id int,
+ders_adi varchar(10),
+yazili_notu int,
+constraint deneme_fk foreign key(ogrenci_id )
+references lise(id)
+);
 /*-------------------------------------------------------------------------
 4) deneme_puani tablosuna kayitlari ekleyin
  ('123','kimya',75), 
@@ -28,10 +52,22 @@ ogrenci_id field'i Foreign Key olsun
  ('128','kimya',93),
  ('129','fizik',25)
 --------------------------------------------------------------------------*/
+insert into deneme_puani values
+('123','kimya',75), 
+ ('124','fizik',65),
+ ('125','tarih',90),
+ ('126','kimya',87),
+ ('127','tarih',69),
+ ('128','kimya',93),
+ ('129','fizik',25);
+ 
+ select *from lise;
+ select *from deneme_puani;
 /*-------------------------------------------------------------------------
 5) Ismi Resul Can olan ogrencinin notunu notlar tablosundaki 
 ogrenci id'si 129 olan yazili notu ile update edin. 
 --------------------------------------------------------------------------*/
+
 /*-------------------------------------------------------------------------
 6) Ders adi fizik olan kayitlarin yazili notunu Oguz Karaca'nin grade'i
 ile update edin. 
